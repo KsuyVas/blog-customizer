c28e690265994d068ff5dbf8986436c30386cc2c
Ksu Sal <ksuyvas@yandex.ru>
2026-06-07 02:38:11 +0300

    fix: restore original package.json and storybook config, fix dependencies conflict

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,35 @@
-import { CSSProperties } from 'react';
+import { useState, CSSProperties } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './../../constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [appliedStyles, setAppliedStyles] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appliedStyles.fontFamilyOption.value,
+					'--font-size': appliedStyles.fontSizeOption.value,
+					'--font-color': appliedStyles.fontColor.value,
+					'--container-width': appliedStyles.contentWidth.value,
+					'--bg-color': appliedStyles.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				onApply={setAppliedStyles}
+				currentStyles={appliedStyles}
+			/>
 			<Article />
 		</main>
 	);
